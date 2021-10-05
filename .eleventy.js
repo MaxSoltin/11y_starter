2aa5901d515d980bb214492c75861e763e7738d1
Max Soltin <timax.pxu@gmail.com>
2021-10-05 14:09:17 +0300

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,28 @@
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy('src/assets');
+	eleventyConfig.addPassthroughCopy('src/css')
+	eleventyConfig.addPassthroughCopy('src/js')
+	eleventyConfig.addPassthroughCopy('src/fonts')
+	eleventyConfig.addPassthroughCopy('src/img')
+	eleventyConfig.addPassthroughCopy('src/content/images/**/*.(jpg|png|webp|svg)')
+
+	const markdownIt = require('markdown-it')
+
+	let options = {
+		html: true,
+		linkify: true,
+	}
+
+	eleventyConfig.setLibrary('md', markdownIt(options))
 
 	return {
-		addPassthroughFileCopy: true,
-		markdownTemplateEngine: 'njk',
-		templateFormats: ['html', 'njk', 'md'],
 		dir: {
 			input: 'src',
 			output: 'dist',
 			includes: 'includes',
 			layouts: 'layouts',
 		},
-	};
-};
+		markdownTemplateEngine: 'njk',
+		passthroughFileCopy: true,
+		templateFormats: ['njk', 'md'],
+	}
+}
---
+++ src/content/Post_1/index.md
@@ -0,0 +1,8 @@
+---
+layout: 'post.njk'
+title: 'Post 1'
+permalink: 'post_1/index.html'
+tags: 'posts'
+---
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores asperiores minima ab cumque, id non perspiciatis eum? Alias ex quam nesciunt facilis, debitis laborum totam ad laudantium sunt nihil magni?
